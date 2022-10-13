a213dd0efd4377d7561446dd4c6411ba74ad6707
xenon <pmcruz@hermenautas.com>
2022-10-13 15:23:33 +0200

    sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import AboutUs from './pages/AboutUs'
+import Index from './pages/Index'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />
+      },
+      {
+        path: '/about-us',
+        element: <AboutUs />
+      }
+    ] 
+  } 
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
