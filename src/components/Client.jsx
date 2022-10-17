894c3ab43d139ae7cab334ed3954eba2cf79cd36
xenon <pmcruz@hermenautas.com>
2022-10-17 11:14:15 +0200

    clients tab

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -7,14 +7,28 @@ function Client({data}) {
   return (
     <>
       <tr className='border-b'>
-        <td className='p-6'>
-          {nombre}
+        <td className='p-6 space-y2'>
+          <p className='text-2xl text-gray-800'>{nombre}</p>
+          <p>{empresa}</p>
         </td>
         <td className='p-6'>
-          {telefono}
+          <p className='text-gray-600'><span className='text-gray-800 uppercase font-bold'>Email: </span>{email}</p>
+          <p className='text-gray-600'><span className='text-gray-800 uppercase font-bold'>Teléfono: </span>{telefono}</p>
         </td>
-        <td className='p-6'>
-          {empresa}
+        <td className='p6 flex gap-3'>
+          <button
+            type='button'
+            className='text-blue-600 hover:text-blue-700 uppercase fonto-bold text-xs'
+          >
+            Edit            
+          </button>
+
+          <button
+            type='button'
+            className='text-red-600 hover:text-red-700 uppercase font-bold text-xs'
+          >
+            Eliminar  
+          </button>
         </td>
       </tr>                              
     </>
