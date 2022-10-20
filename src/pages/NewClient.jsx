cfad3bdf9ef7d4a6674813b960d677cf2540f67b
xenon <pmcruz@hermenautas.com>
2022-10-20 17:37:57 +0200

    form

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function NewClient() {
+
+  const navigate = useNavigate()
+
   return (
-    <div>New Client</div>
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>New Client</h1>
+      <p className='mt-3'>Fill in all the fields to register a new client</p>
+    
+      <div className='flex justify-end'>
+        <button
+          className='bg-blue-800 text-white px-3 py-1 font-bold uppercase'
+          onClick={() => navigate('/')}
+        >
+          back  
+        </button>
+      </div>
+
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10'>
+        <p>Formulario Aquí</p>
+      </div>
+    </>
   )
 }
 
